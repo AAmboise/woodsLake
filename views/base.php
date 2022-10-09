feba7533707d139ffb28e07407453d049ced2936
AAmboise <adrien.amboise@yahoo.fr>
2022-10-09 17:10:41 +0200

    remove jquery  & SLICK CDN

--- views/base.php
+++ views/base.php
@@ -8,15 +8,19 @@ ini_set('display_errors',true);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- CSS only -->
+
+    <!-- BOOTSTRAP -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    
     <!-- FONTAWESOME -->
     <script src="https://kit.fontawesome.com/ac64632661.js" crossorigin="anonymous"></script>
+
     <!-- SLICK -->
     <link rel="stylesheet" type="text/css" href="../assets/js/slick/slick.css"/>
-        <!-- Add the new slick-theme.css if you want the default styling -->
+    <!-- Add the new slick-theme.css if you want the default styling -->
     <link rel="stylesheet" type="text/css" href="../assets/js/slick/slick-theme.css"/>
-    <!-- Lightbox -->
+
+    <!-- LIGHTBOX -->
     <link rel="stylesheet" type="text/css" href="../assets/css/lightbox.min.css"/>
 
 
@@ -50,16 +54,14 @@ ini_set('display_errors',true);
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 
 <!-- JQUERY -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<!-- <script type="text/javascript" src="../assets/js/jquery-3.6.1.min.js"></script> -->
-<!-- <script type="text/javascript" src="../assets/js/jquery-migrate-1.4.1.min.js"></script> -->
+<script type="text/javascript" src="../assets/js/jquery-3.6.1.min.js"></script>
+<script type="text/javascript" src="../assets/js/jquery-migrate-1.4.1.min.js"></script>
 
 <!-- SLICK -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
-<!-- <scrip type="text/javascript" src="../assets/js/slick/slick.min.js"></script> -->
+<script type="text/javascript" src="../assets/js/slick/slick.min.js"></script>
+
 <!-- Lightbox -->
 <script src="../assets/js/lightbox.js"></script>
-
 
 <!-- SCRIPT SPECIFIQUE -->
 <?=$script?>
