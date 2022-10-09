b5639ebc976886d11a3af7b22381be9e01ea0634
AAmboise <adrien.amboise@yahoo.fr>
2022-10-09 17:24:08 +0200

    remove bootstrap CDN

--- views/base.php
+++ views/base.php
@@ -9,9 +9,9 @@ ini_set('display_errors',true);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <!-- BOOTSTRAP -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    
+    <!-- BOOTSTRAP 5.2.2-->
+    <link rel="stylesheet" href="../assets/css/bootstrap/bootstrap.min.css">
+
     <!-- FONTAWESOME -->
     <script src="https://kit.fontawesome.com/ac64632661.js" crossorigin="anonymous"></script>
 
@@ -50,14 +50,14 @@ ini_set('display_errors',true);
         
     </div>
     
-<!-- BOOTSTRAP -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+<!-- BOOTSTRAP 5.2.2-->
+<script type="text/javascript" src="../assets/js/bootstrap/bootstrap.bundle.min.js"></script>
 
-<!-- JQUERY -->
+<!-- JQUERY 3.6.1 -->
 <script type="text/javascript" src="../assets/js/jquery-3.6.1.min.js"></script>
 <script type="text/javascript" src="../assets/js/jquery-migrate-1.4.1.min.js"></script>
 
-<!-- SLICK -->
+<!-- SLICK 1.8.1-->
 <script type="text/javascript" src="../assets/js/slick/slick.min.js"></script>
 
 <!-- Lightbox -->
